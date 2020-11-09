63c5a8d212a74600de5f6ffe383d3cced0072c1f
Rouncer27 <trevor@switchbackcreative.ca>
2020-11-09 14:14:28 -0700

    minor changes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
 import { ThemeProvider } from "styled-components"
 
 import theme from "../styles/theme/Theme"
@@ -9,20 +8,8 @@ import Footer from "./Footer"
 
 const Layout = props => {
   const children = props.children
-
   const headerNeeded =
     props.location === "/privacy-policy" || props.location === "/disclaimer"
-  console.log(headerNeeded)
-
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
 
   return (
     <>
